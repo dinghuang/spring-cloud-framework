6acd512b59bfe8130454f56c46bddc6d4669d9b2
丁煌 <huang.ding@hand-china.com>
2020-01-16 11:35:50 +0800

    [ADD] pingan

--- dinghuang-framework-parent/dinghuang-framework-core/src/main/java/org/dinghuang/core/security/SecurityContextHystrixRequestVariableSetterFilter.java
+++ dinghuang-framework-parent/dinghuang-framework-core/src/main/java/org/dinghuang/core/security/SecurityContextHystrixRequestVariableSetterFilter.java
@@ -1,33 +1,33 @@
-package org.dinghuang.core.security;
-
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import javax.servlet.*;
-import java.io.IOException;
-
-/**
- * @author dinghuang123@gmail.com
- * @since 2019/6/10
- */
-public class SecurityContextHystrixRequestVariableSetterFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        if (SecurityContextHolder.getContext().getAuthentication() != null) {
-            SecurityContextHystrixRequestVariable.getInstance().set(SecurityContextHolder.getContext());
-        }
-        chain.doFilter(request, response);
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
-        //do not something
-    }
-
-    @Override
-    public void destroy() {
-        //do not something
-    }
-}
-
+//package org.dinghuang.core.security;
+//
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+//import javax.servlet.*;
+//import java.io.IOException;
+//
+///**
+// * @author dinghuang123@gmail.com
+// * @since 2019/6/10
+// */
+//public class SecurityContextHystrixRequestVariableSetterFilter implements Filter {
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+//        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+//            SecurityContextHystrixRequestVariable.getInstance().set(SecurityContextHolder.getContext());
+//        }
+//        chain.doFilter(request, response);
+//    }
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) {
+//        //do not something
+//    }
+//
+//    @Override
+//    public void destroy() {
+//        //do not something
+//    }
+//}
+//
